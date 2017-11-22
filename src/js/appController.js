4de94c0be4df6c24ccd95a8040d78ec3b037817c
christianraymond <ombenisn.raymond@gmail.com>
2017-11-22 16:16:48 +0200

    changed the layouts

--- src/js/appController.js
+++ src/js/appController.js
@@ -21,8 +21,8 @@ define(['ojs/ojcore', 'knockout', 'ojs/ojrouter', 'ojs/ojknockout', 'ojs/ojarray
        self.router = oj.Router.rootInstance;
        self.router.configure({
          'dashboard': {label: 'Dashboard', isDefault: true},
-         'incidents': {label: 'Incidents'},
-         'customers': {label: 'Customers'},
+        //  'incidents': {label: 'Incidents'},
+        //  'customers': {label: 'Customers'},
          'about': {label: 'About'}
        });
       oj.Router.defaults['urlAdapter'] = new oj.Router.urlParamAdapter();
@@ -31,12 +31,12 @@ define(['ojs/ojcore', 'knockout', 'ojs/ojrouter', 'ojs/ojknockout', 'ojs/ojarray
       var navData = [
       {name: 'Dashboard', id: 'dashboard',
        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24'},
-      {name: 'Incidents', id: 'incidents',
-       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-fire-icon-24'},
-      {name: 'Customers', id: 'customers',
-       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-people-icon-24'},
-      {name: 'About', id: 'about',
-       iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'}
+      // {name: 'Incidents', id: 'incidents',
+      //  iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-fire-icon-24'},
+      // {name: 'Customers', id: 'customers',
+      //  iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-people-icon-24'},
+      // {name: 'About', id: 'about',
+      //  iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-info-icon-24'}
       ];
       self.navDataSource = new oj.ArrayTableDataSource(navData, {idAttribute: 'id'});
 
--- src/js/jet-composites/all-orders/viewModel.js
+++ src/js/jet-composites/all-orders/viewModel.js
@@ -10,7 +10,7 @@ define(
         var self = this;
         self.composite = context.element;
         //Example observable
-        self.messageText = ko.observable('Hello from Example Component');
+        self.messageText = ko.observable('ALL ORDERS');
 
         context.props.then(function (propertyMap) {
             //Store a reference to the properties for any later use
@@ -19,6 +19,43 @@ define(
             //Parse your component properties here
 
         });
+
+        self.newOrders = ko.observableArray([
+          {id : 123, header : "One", desc : "Order from Canada!"},
+          {id : 765, header : "Two", desc : "Another order!"},
+          {id : 829, header : "Three", desc : "New order coming!"},
+          {id : 469, header : "Four", desc : "New order coming!"},
+          {id : 569, header : "Five", desc : "New order coming!"},
+          {id : 169, header : "Six", desc : "New order coming!"},
+        ]);
+
+        self.ordersInProcess = ko.observableArray([]);
+
+        self.completedOrders = ko.observableArray([]);
+
+        self.processOrder = function(order){
+
+          self.ordersInProcess.push(order);
+
+          var orderList = self.newOrders().filter(function(o){
+              return o.id !== order.id;
+          });
+
+          self.newOrders(orderList);
+        }
+
+        self.completeOrder = function(order){
+
+          self.completedOrders.push(order);
+
+          var processedOrders = self.ordersInProcess().filter(function(p){
+            return p.id !== order.id;
+          })
+
+          self.ordersInProcess(processedOrders)
+
+        }
+
     };
 
     //Lifecycle methods - uncomment and implement if necessary
